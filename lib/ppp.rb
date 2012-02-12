c2ebe1820f3da8f8aa5526f40df989bf9afea02f
Jon Sangster <jon@ertt.ca>
2012-02-12 15:08:46 -0800

    Add documentation. Rename client.rb to generator.rb

--- lib/ppp.rb
+++ lib/ppp.rb
@@ -1,18 +1,24 @@
 require 'digest'
 require "ppp/version"
-require "ppp/client"
+require "ppp/generator"
 require 'ppp/Cppp'
 
 module Ppp
   class << self
-    def new key
-      client = Client.new key
+  @@ALPHABETS   = { :conservative => '!#%+23456789:=?@ABCDEFGHJKLMNPRSTUVWXYZabcdefghijkmnopqrstuvwxyz',
+                    :aggressive   => '!"#$%&\'()*+,-./23456789:;<=>?@ABCDEFGHJKLMNOPRSTUVWXYZ[\]^_abcdefghijkmnopqrstuvwxyz{|}~' }
+    
+    # @return [Ppp::Generator] with the given SHA-256 key
+    def new key, length=4, alphabet=@@ALPHABETS[:conservative]
+      Generator.new key, length, alphabet
     end
 
+    # @return a SHA-256 digest of the given string
     def key_from_string str
       Digest::SHA256.new.update( str ).to_s
     end
 
+    # @return a random SHA-256 key
     def random_key
       Cppp.random_key
     end
---
+++ lib/ppp/generator.rb
@@ -0,0 +1,43 @@
+require 'ppp/Cppp'
+
+# Generates passcodes.
+class Ppp::Generator
+  @@HEX_PATTERN = /[a-fA-F0-9]{64}/
+
+  # @param [String] sha256_key a 64 hex-digit string representation of a
+  #   SHA-256 hash. This hash will seed all the generated passcodes.
+  # @param [Fixnum] length the number of characters in each generated passcode
+  # @param [String] alphabet a string containing the characters passcodes will
+  #   be comprised of. Cannot contain null characters and duplicate characters
+  #   will be removed.
+  def initialize sha256_key, length, alphabet
+    raise "Expected a 64 digit hex-string, got \"#{key}\"" if @@HEX_PATTERN.match( sha256_key ).nil?
+    raise "alphabet cannot contain null character" if alphabet.include? ?\0
+
+    @seed     = sha256_key
+    @length   = length
+    @alphabet = alphabet.split( '' ).uniq.join
+  end
+
+  # Creates passcodes seeded off the SHA-256 key this object was created with.
+  #   Calling this method subsequent times with the same offset will return the
+  #   same passcodes, so you should increase the offset by count each time.
+  # @param [Fixnum] offset the number of passcodes to skip
+  # @param [Fixnum] count the number of passcodes to return
+  # @return [Array] an array of passcodes
+  def passcodes offset, count
+    Cppp.passcodes @seed, offset, count, @length, @alphabet
+  end
+
+  # (@see #passcodes)
+  def passcode offset
+    passcodes( offset, 1 ).first
+  end
+
+  # Check if a given passcode is correct
+  # @param [Fixnum] index the index of the passcode to check against
+  # @return [Boolean] if the given passcode matches the passcode at the given offset
+  def verify index, given_passcode
+    passcode( index ) == given_passcode
+  end
+end
