3386a11f9b97b475e15a7c6e5b086cd8dc647eae
Jon Sangster <jon@ertt.ca>
2012-02-13 20:40:50 -0800

    Switch DIV card to TABLE card

--- lib/ppp/card_printer.rb
+++ lib/ppp/card_printer.rb
@@ -2,7 +2,7 @@ require 'ppp/generator'
 
 class Ppp::CardPrinter
 
-  @@COLUMN_NAMES = ?A...?Z
+  @@FIRST_COLUMN = ?A
   @@CHARS_PER_LINE = 34
   @@SIZES = {:creditcard => %w[ 85.60mm 53.98mm ]}
 
@@ -19,7 +19,7 @@ class Ppp::CardPrinter
   end
 
   def passcodes_per_line
-    ( (@@CHARS_PER_LINE) / (@generator.length + 1) ).to_i
+    @passcodes_per_line ||= ( (@@CHARS_PER_LINE+1) / (@generator.length + 1) ).to_i
   end
   
   def next_code
@@ -31,7 +31,7 @@ class Ppp::CardPrinter
   def css
     %[
       <style>
-        .card, .card .title, .card .card_index, .card .codes, .card .codes li {
+        .card, .card .title, .card .card_index, .card .codes, .card .codes td {
           font-family: monospace;
           font-size: 3.5mm;
         }
@@ -41,50 +41,76 @@ class Ppp::CardPrinter
           width:  #{width};
           height: #{height};
           border: 1px solid \#000;
+          border-top: none;
+          border-spacing: 0;
         }
         .title {
-          border-bottom: 1px solid \#aaa;
+          border: 1px solid \#000;
+          border-bottom: 1px solid \#000;
           background-color: \#eee;
+          text-align: left;
+          padding-left: 1ex;
+          font-weight: bold;
+        }
+        .title td:first-child {
+          width: 2.5em;
         }
         .card_index {
           float: right;
           margin-right: 1ex;
         }
-        .codes {
-        }
-        .codes ol {
-          padding: 0;
-          margin: 0;
-          margin-left: 3em;
-          list-style-position: outside;
-        }
-        .codes .codes_heading {
-          margin-left: 3em;
-        }
-        .codes .codes_heading .codes_column {
+        .card_index:before { content: '['; }
+        .card_index:after  { content: ']'; }
+
+        .codes_heading th {
           text-align: center;
           width: #{@generator.length}em;
+          border-bottom: 1px solid \#aaa;
+          background-color: \#eee;
         }
+        .codes_heading th:first-child {
+          background-color: \#fff;
+          border-right: 1px solid \#aaa;
+        }
+        .codes_heading th:last-child  { border-right:  none; }
+        .codes td {
+          padding: 0;
+          margin: 0;
+          text-align: center;
+          border-right: 1px solid \#aaa;
+          border-bottom: 1px solid \#aaa;
+        }
+        .codes tr td:first-child {
+          text-align: right;
+          padding-right: 0.5em;
+          font-weight: bold;
+          background-color: \#eee;
+          border-right: 1px solid \#aaa;
+        }
+        .codes tr td:first-child { border-bottom: none; }
+        .codes tr td:last-child  { border-right:  none; }
+        .codes tr:last-child td  { border-bottom: none; }
       </style>
     ]
   end
 
   def html
     %[
-      <div class="card">
-        <div class="title">
-        #{@title}
-        <span class="card_index">#{@card_index}</span>
-        </div>
-        <div class="codes">
-          <div class="codes_heading">
-            #{ @@COLUMN_NAMES.first(passcodes_per_line).collect { |c| %[<span class="codes_column">#{c}</span>] }.join ?\n }
-          </div>
-          <ol>
-            #{ (0..9).collect{ "<li>#{(0..passcodes_per_line).collect {next_code}.join(' ') }</li>" }.join(?\n) }
-          </ol>
-        </div>
-      </div>
+      <table class="card">
+        <caption class="title">
+          #{@title}
+          <span class="card_index">#{@card_index}</span>
+        </caption>
+        <colgroup span="1">
+        <colgroup span="#{passcodes_per_line}">
+        <thead class="codes_heading">
+          <th>&nbsp;</th>
+          #{ (0..passcodes_per_line-1).collect { |i| %[<th>#{(@@FIRST_COLUMN.ord + i).chr}</th>] }.join ?\n }
+        </thead>
+        <tbody class="codes">
+          #{ (1..9).collect { |i| "<tr><td>#{i}</td>#{(1..passcodes_per_line).collect {%[<td>#{next_code}</td>]}.join(?\n) }</tr>" }.join(?\n) }
+        </tbody>
+      </table>
     ]
   end
 
